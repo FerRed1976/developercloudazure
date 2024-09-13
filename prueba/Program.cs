3eeba80f96d88d0248bb0e88eca8b7c39c2caf1c
Fernando Rojo <fernando.rojoaranda@outlook.es>
2024-09-13 11:48:15 +0200

    Prueba de evaluacion

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -2,6 +2,14 @@
 //Console.Write("Congratulation");
 // Console.Write(" ");
 //Console.Write("Estoy aqui");
-var dados = new Random ();
-var roll = dados.Next(1,7);
-Console.WriteLine (roll);
+
+//RANDOM
+//var dados = new Random ();
+//var roll = dados.Next(1,7);
+//Console.WriteLine (roll);
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue,secondValue);
+Console.WriteLine(largerValue);
