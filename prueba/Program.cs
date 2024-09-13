a1c9db81c322a6f4f21718b6da132f9cc59b825a
Fernando Rojo <fernando.rojoaranda@outlook.es>
2024-09-13 13:30:56 +0200

    if or else

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -8,8 +8,26 @@
 //var roll = dados.Next(1,7);
 //Console.WriteLine (roll);
 
-int firstValue = 500;
-int secondValue = 600;
-int largerValue;
-largerValue = Math.Max(firstValue,secondValue);
-Console.WriteLine(largerValue);
+//Prueba de Evaluacion
+//int firstValue = 500;
+//int secondValue = 600;
+//int largerValue;
+//largerValue = Math.Max(firstValue,secondValue);
+//Console.WriteLine(largerValue);
+
+//If Else
+
+//Random dice = new Random();
+
+//int roll1 = dice.Next(1, 7);
+//int roll2 = dice.Next(1, 7);
+//int roll3 = dice.Next(1, 7);
+
+//int total = roll1 + roll2 + roll3;
+
+//Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+
+var total = 0
+int[] tirada=new int[3];
+Console.WriteLine(${tirada[i]};{(i<2 ? "+" : "")}")
