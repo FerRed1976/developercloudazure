18a6fa6314bf5f420f384d4b95450247a3e3be46
Fernando Rojo <fernando.rojoaranda@outlook.es>
2024-09-17 12:39:03 +0200

    otro

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,39 +1,6 @@
-﻿
+﻿Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
 
-string sku = "01-MN-L";
-
-string[] product = sku.Split('-');
-
-string type = "";
-string color = "";
-string size = "";
-
-switch (producto)
-{
-  case 02:
-  type = "Sweat shirt";
-  break ;
-  case 03:
-  type = "T-Shirt";
-  break;
-   
-  case "BL":
-  color = "Black";
-  break;
-  case "MN":
-  color = "Maroon"
-  break;
-  case"W":
-  color = "White"
-  break;
-
-  case "s":
-  size = "Small";
-  break;
-  case "M":
-  size = "Medium";
-  break;
-  case "L":
-  size = "Large";
-}
-Console.WriteLine($"Product: {size} {color} {type}");
+string myValue = "a";
+Console.WriteLine(myValue == "a");
